3db2e5e713d03cb62b7cf68f07009a12551e0a70
ZZ7R2M@allyipoc.com <ZZ7R2M@allyipoc.com>
2018-06-19 11:01:01 -0400

    Added create, update, delete functionality

--- src/main/java/com/el/jokes_mvc/controllers/JokesController.java
+++ src/main/java/com/el/jokes_mvc/controllers/JokesController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.el.jokes_mvc.models.Joke;
@@ -28,5 +29,60 @@ public class JokesController {
 		mv.setViewName("read");
 		return mv;
 	}
+	@GetMapping("/new_joke")
+	public ModelAndView new_joke() {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("new_joke");
+		return mv;
+	}
+	
+	@PostMapping("/new_joke")
+	public ModelAndView create_joke(String joke, String punchline, int rating) {
+		Joke newJoke = new Joke(joke, punchline, rating);
+		jokeRepository.save(newJoke);
+    	ModelAndView mv = new ModelAndView();
 
+		mv.setViewName("redirect:/");
+
+	    return mv;
+    }
+
+	@PostMapping("/delete_joke")
+	public ModelAndView delete_joke(int jokeID) {
+
+		//Joke joke = jokeRepository.getOne(jokeID);
+		jokeRepository.delete(jokeID);
+    	ModelAndView mv = new ModelAndView();
+
+		mv.setViewName("redirect:/");
+
+	    return mv;
+    }
+	
+	@PostMapping("/update_joke")
+	public ModelAndView update_joke(int jokeID) {
+		ModelAndView mv = new ModelAndView();
+		Joke update_joke = jokeRepository.findOne(jokeID);
+		String joke = update_joke.getJoke();
+		String punchline = update_joke.getPunchline();
+		int rating = update_joke.getRating();
+		mv.addObject("joke", joke);
+		mv.addObject("punchline", punchline);
+		mv.addObject("rating", rating);
+		mv.addObject("id", jokeID);
+		mv.setViewName("update_joke");
+		return mv;
+	}
+	
+	@PostMapping("/update")
+	public ModelAndView update(String joke, String punchline, int rating, int id) {
+		ModelAndView mv = new ModelAndView();
+		Joke updated_joke = jokeRepository.findOne(id);
+		updated_joke.setJoke(joke);
+		updated_joke.setPunchline(punchline);
+		updated_joke.setRating(rating);
+		jokeRepository.save(updated_joke);
+		mv.setViewName("redirect:/");
+		return mv;
+	}
 }
